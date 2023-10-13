3cab008c995085034f2099acb0dbe363d02cf499
Pascal <psibondagara@outlook.com>
2023-10-13 08:36:49 +0200

    Added main to Gitnore

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ import './theme/variables.css';
 // Your web app's Firebase configuration
 //Bytt ut 2 med 4 viss du vil prøve
 const firebaseConfig = {
-  apiKey: "AIzaSyBufYAA0xdS4G5A6W5aOpowqHSBNj4dLH2",
+  apiKey: "AIzaSyBufYAA0xdS4G5A6W5aOpowqHSBNj4dLH4",
   authDomain: "formula-30243.firebaseapp.com",
   projectId: "formula-30243",
   storageBucket: "formula-30243.appspot.com",
